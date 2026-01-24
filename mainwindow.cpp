3f7209aa33f7759d4805757c95663d46680d2713
KrzysztofRostek <kr316302@student.polsl.pl>
2026-01-24 23:37:35 +0100

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -339,16 +339,21 @@ void MainWindow::on_RESET_Button_clicked()
     symulator.reset();
 
     symulator.setPID_Kp(0);
+    ui->spinBOX_WzmocK->setValue(0);
     symulator.setPID_Ti(0);
+    ui->spinBOX_Ti->setValue(0);
     symulator.setPID_Td(0);
+    ui->spinBOX_Td->setValue(0);
     symulator.setGeneratorA(0);
+    ui->spinBOX_Amplituda->setValue(0);
     symulator.setGeneratorTRZ(0);
-    symulator.setGeneratorTT(0);
+    ui->spinBOX_Czstotliwosc->setValue(0);
+    symulator.setGeneratorTT(200);
+    ui->spinBOX_Interwal->setValue(200);
 
     symulator.setPID_TypCalki(RegulatorPID::ZERO);
     ui->radio_przed->setChecked(false);
     ui->radio_pod->setChecked(false);
-
     aktualnyWektorA = {0};
     aktualnyWektorB = {0};
     aktualneOpoznienie = 1;
